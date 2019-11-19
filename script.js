8af4541eca3597abd21f25631bb3a5690c300827
Selchuk Karakus <selchuck.karakus@gmail.com>
2019-11-19 10:44:36 +0000

    clean up files

--- script.js
+++ script.js
@@ -2,15 +2,34 @@ let buttonEnter = document.getElementById("btn-enter");
 let buttonClear=document.getElementById("btn-clear");
 let input = document.getElementById("userinput");
 const ul = document.querySelector("ul");
+let button =document.createElement("button");
 
 function inputLength () {
   return input.value.length
 }
 
 function createListElement() {
-  var li = document.createElement("li");
+  let li = document.createElement("li");
   li.appendChild(document.createTextNode(input.value));
   ul.appendChild(li);
+
+  li.addEventListener("click", function() {
+    let finished = this.classList.toggle("done");
+    let removeButton = document.createElement("button");
+    removeButton.classList.add("deleteButton");
+
+  if (finished) {
+      removeButton.appendChild(document.createTextNode("remove?"));
+      removeButton.classList = "deleteButton";
+      li.appendChild(removeButton);
+
+      removeButton.addEventListener("click", function() {
+        this.parentElement.remove();
+      });
+    } else {
+      this.getElementsByClassName("deleteButton")[0].remove();
+    }
+  })
   input.value = "";
 }
 
@@ -29,6 +48,7 @@ function addListAfterKeypress(event) {
 function clearList() {
     ul.innerHTML = "";
 }
+
 
 buttonClear.addEventListener("click", clearList);
 buttonEnter.addEventListener("click", addListAfterClick);
